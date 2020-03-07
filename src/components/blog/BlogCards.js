8f05683c76d65364d8aa871cb3dd4eca038b24ad
https://github.com/ayomidebajo <oluwashinabajo@gmail.com>
2020-03-07 19:44:50 +0100

    remove second blog card

--- src/components/blog/BlogCards.js
+++ src/components/blog/BlogCards.js
@@ -12,32 +12,12 @@ class BlogCards extends Component {
               <img
                 className="blog--image"
                 width="100%"
+                height="100%"
                 src={Img1}
                 alt="e-commerce store"
               />
             </div>
             <h3>Pushing an existing repository to Github.</h3>
-            <a
-              className="blog--link"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://dev.to/ayomide_bajo/pushing-an-existing-repository-to-github-1f68"
-            >
-              <button className="btn btn--black">Read article</button>
-            </a>
-          </div>
-
-          <div className="blog--card__container">
-            <div className="blog--card">
-              <img
-                className="blog--image"
-                width="100%"
-                src={Img1}
-                alt="e-commerce store"
-              />
-            </div>
-            <h3>Pushing an existing repository to Github.</h3>
-
             <a
               className="blog--link"
               rel="noopener noreferrer"
